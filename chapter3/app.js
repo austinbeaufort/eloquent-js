916803ee60ddbfb42cb8ea8069c02760c20a674d
Austin Beaufort <tinyhatcoding@gmail.com>
2019-02-04 19:30:51 -0500

    finished Ch.3 and Most of Ch.4

--- chapter3/app.js
+++ chapter3/app.js
@@ -76,7 +76,192 @@
 // hummus(2);
 
 
-let greet = who => console.log(`Hello ${who}!`);
+// let greet = who => console.log(`Hello ${who}!`);
 
-greet('Harry');
-console.log('bye');
+// greet('Harry');
+
+// console.log('bye');
+
+
+// PASSING TOO MANY ARGUMENTS, EXTRAS WILL BE IGNORED.
+// PASSING TOO FEW, LEFT OVER PARAMETERS BECOME UNDEFINED.
+
+// function minus(a, b) {
+//     if (b === undefined) return -a;
+//     else return a - b;
+// }
+
+// console.log(minus(10)); // -10
+// console.log(minus(10, 5)); // 5
+
+
+
+// DEFAULT PARAMETERS
+
+// function power(base, exponent = 2) {
+//     let result = 1;
+//     for (let count = 0; count < exponent; count++) {
+//         result *= base;
+//     }
+//     return result;
+// }
+
+// console.log(power(4)); // 16
+// console.log(power(2, 6)); // 64
+
+
+// CLOSURE
+
+
+// FUNCTIONS CAN RETURN VALUES TO PASS TO OTHER VARIABLES / FUNCTIONS
+// function wrapValue(n) {
+//     let local = n;
+//     return () => local;
+// }
+
+// let wrap1 = wrapValue(1);
+// let wrap2 = wrapValue(2);
+
+// console.log(wrap1()); // 1
+// console.log(wrap2()); // 2
+
+
+// function multiplier(factor) {
+//     return number => number * factor;
+// }
+
+// // multiplier = factor => {
+// //     return number => number * factor;
+// // }
+
+// let twice = multiplier(2);
+// let timesThree = multiplier(3);
+// console.log(twice(5)); // 10
+// console.log(timesThree(9)); // 27
+
+
+// RECURSION
+
+// function power(base, exponent) {
+//     if (exponent == 0) {
+//         return 1;
+//     } else {
+//         return base * power(base, exponent - 1);
+//     }
+// }
+
+// console.log(power(3, 4));
+
+
+// function findSolution(target) {
+//     function find(current, history) {
+//         if (current == history) {
+//             return history;
+//         } else if (current > target) {
+//             return null;
+//         } else {
+//             return find(current + 5, `(${history} + 5)`) ||
+//                    find(current * 3, `(${history} * 3`);
+//         }
+//     }
+//     return find(1, '1');
+// }
+
+// console.log(findSolution(24));
+
+
+// function printFarmInventory(cows, chickens) {
+//     let cowString = String(cows);
+//     while(cowString.length < 3) {
+//         cowString = '0' + cowString;
+//     }
+//     console.log(`${cowString} Cows`);
+
+//     let chickenString = String(chickens);
+//     while (chickenString.length < 3) {
+//         chickenString = '0' + chickenString;
+//     }
+//     console.log(`${chickenString} Chickens`);
+// }
+
+// printFarmInventory(7, 11);
+
+
+
+// function printZeroPaddedWithLabel(number, label) {
+//     let numberString = String(number);
+//     while (numberString.length < 3) {
+//         numberString = '0' + numberString;
+//     }
+//     console.log(`${numberString} ${label}`);
+// }
+
+// function printFarmInventory(cows, chickens, pigs) {
+//     printZeroPaddedWithLabel(cows, 'Cows');
+//     printZeroPaddedWithLabel(chickens, 'Chickens');
+//     printZeroPaddedWithLabel(pigs, 'Pigs');
+// }
+
+// printFarmInventory(7, 11, 3);
+
+
+// function zeroPad(number, width) {
+//     let string = String(number);
+//     while (string.length < width) {
+//         string = '0' + string;
+//     }
+//     return string;
+// }
+
+// function printFarmInventory(cows, chickens, pigs) {
+//     console.log(`${zeroPad(cows, 3)} Cows`);
+//     console.log(`${zeroPad(chickens, 3)} Chickens`);
+//     console.log(`${zeroPad(pigs, 3)} Pigs`);
+// }
+
+// printFarmInventory(7, 16, 3);
+
+
+
+// EXERCISE: RETURN MINIMUM
+
+// findMin = (a, b) => Math.min(a, b);
+
+// console.log(findMin(-20, 36));
+
+
+
+// EXERCISE: RECURSION WITH EVEN AND ODD
+
+// isEven = number => {
+//     if(number == 0) return true;
+//     if(number == 1) return false;
+//     if(number > 1) return isEven(number - 2);
+// }
+
+// console.log(isEven(77));
+// console.log(isEven(78));
+
+
+// EXERCISE: BEAN COUNTING
+
+// countBs = string => {
+//     let counter = 0;
+//     for(let i = 0; i < string.length; i++) {
+//         if (string.charAt(i) == 'B') counter ++;
+//     }
+//     return counter;
+// }
+
+// console.log(countBs('the Bat cannot Bite the Bitter Butter Because it is too Bitter'));
+
+
+// countChar = (string, letter) => {
+//     let counter = 0;
+//     for (let i = 0; i < string.length; i++) {
+//         if (string.charAt(i) == letter) counter++;
+//     }
+//     return counter;
+// }
+
+// console.log(countChar('The cat ran across the crooked lawn.', 'c'));
---
+++ chapter4/app.js
@@ -0,0 +1,234 @@
+// let day1 = {
+//     squirrel: false,
+//     events: ['work', 'touched tree', 'pizza', 'running']
+// };
+
+// day1.wolf = false;
+// console.log(day1.squirrel);
+// console.log(day1.wolf);
+// console.log(day1.events[2]); // pizza
+
+// to delete a property from an object,
+// delete day1.wolf;
+// console.log(day1.wolf); // undefined.
+
+// console.log('events' in day1); // exists, so true.
+// console.log('squirrel' in day1);// exists, so true.
+
+
+// console.log(Object.keys({  // prints out object keys
+//     squirrel: false,
+//     events: ['work', 'touched tree', 'pizza', 'running']
+// }));
+
+
+// let objectA = {a: 1, b:2};
+// Object.assign(objectA, {b: 3, c: 4});
+// console.log(objectA); // {a: 1, b: 3, c: 4};
+
+
+// let journal = []; // creates empty journal
+
+// function addEntry(events, squirrel) { // this function pushes objects to the journal.
+//     journal.push({events, squirrel});
+// }
+
+// addEntry(['work', 'touched tree', 'pizza', 'running', 'television'], false);  // run object through function to push it to journal.
+// addEntry(['work', 'ice cream', 'cauliflower', 'lasagna', 'touched tree', 'brushed teeth'], false);
+// addEntry(['weekend', 'cycling', 'break', 'peanuts', 'beer'], true);
+
+// console.log(journal);
+
+
+// console.log([1, 2, 3, 2, 1].indexOf(2)); // 1, returns the first occurence of '2'.
+// console.log([1, 2, 3, 2, 1].lastIndexOf(2)); // 3, returns the first occurence of '2'.
+
+
+// console.log([0, 1, 2, 3, 4].slice(2, 4)); // [2, 3];
+// console.log([0, 1, 2, 3, 4].slice(2)); // [2, 3, 4] // starts at index 2, returns inclusive 2nd index and everything after.
+
+// let oldArray = [1, 2, 3, 4];
+// let newArray = oldArray.slice(); // creates newArray with all elements of oldArray.
+// console.log(newArray);
+
+
+
+// function remove(array, index) {
+//     return array.slice(0, index) // ['a', 'b']
+//         .concat(array.slice(index + 1)); // concats the return value to index 3 and continues through end of array.
+// }
+
+// console.log(remove['a', 'b', 'c', 'd', 'e'], 2)); // ['a', 'b', 'd', 'e']
+
+
+// console.log('coconuts'.slice(4, 7)); // nut
+// console.log('coconuts'.indexOf('u')); // 5
+
+// console.log('   okay \n  ');
+// console.log('   okay \n  '.trim());
+
+
+// console.log(String(6).padStart(3, '0'));
+
+
+// let sentence = 'Secretarybirds specialize in stomping';
+// let words = sentence.split(' ');
+// console.log(words);
+
+// console.log(words.join(' '));
+
+// console.log('LA '.repeat(7));
+
+
+// REST PARAMETERS
+
+// function max(...numbers) { // ...numbers, will be an array of numbers.
+//     let result = -Infinity;
+//     for(let number of numbers) {
+//         if(number > result) result = number;
+//     }
+//     return result;
+// }
+
+// console.log(max(4, 1, 9, -2)); // 9
+
+// let numbers = [5, 1, 7];
+// console.log(max(...numbers));
+
+
+// let words = ['never', 'fully'];
+// console.log(['will', ...words, 'understand']); // ['will', 'never', 'fully', 'understand'];
+
+
+// function randomPointOnCircle(radius) {
+//     let angle = Math.random() * 2 * Math.PI;
+//     return {x: radius * Math.cos(angle),
+//             y: radius * Math.sin(angle)};
+// }
+
+// console.log(randomPointOnCircle(2));
+
+
+// console.log(Math.floor(Math.random() * 10));
+
+
+// DESTRUCTURING
+
+// let {name, age} = {name: 'Faraji', age: 23};
+// console.log(name, age);
+
+
+
+
+
+// EXERCISE: THE SUM OF A RANGE
+
+// range = (start, end, step = 1) => {
+//     let arr = [];
+
+//     // negative steps
+//     if (step < 0) {
+//         for (let i = start; i >= end; i+=step){
+//             arr.push(i);
+//         }
+//     }
+//     // positive steps
+//     for (let i = start; i <= end; i+=step) {
+//         arr.push(i);
+//     }
+//     return arr;
+// }
+
+// // console.log(range(7, 37));
+
+
+// sum = arr => {
+//     let total = 0;
+//     for (let number of arr) {
+//         total += number;
+//     }
+//     return total;
+// }
+// console.log(sum(range(1, 10, 3)));
+// console.log(sum(range(1, 10)));
+// console.log(sum(range(33, 77)));
+// console.log(sum(range(5, 2, -1)));
+// console.log(sum(range(10, 1, -2)));
+
+
+
+// EXERCISE: REVERSING AN ARRAY
+
+// let startArray = [1, 2, 3, 4, 5];
+
+// let reverseArray = (arr) => {
+//     let newArray = [];
+//     for(let number of arr) {
+//         newArray.unshift(number);
+//     }
+//     return newArray;
+// }
+
+// console.log(reverseArray(startArray));
+
+
+// let startArray = [1, 2, 3, 4, 5];
+// let secondArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let reverseArrayInPlace = (arr) => {
+//     for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+//         let temp = arr[i];
+//         arr[i] = arr[arr.length - i - 1];
+//         arr[arr.length - i - 1] = temp;
+//     }
+//     return arr;
+// }
+
+// console.log(reverseArrayInPlace(startArray));
+// console.log(reverseArrayInPlace(secondArray));
+
+
+
+// EXERCISE: A LIST
+
+let arrayToList = arr => {
+    let list = null;
+    for (let i = arr.length; i >= 1; i--) {
+        list = {value: i, rest: list};
+    }
+    return list;
+}
+
+console.log(arrayToList([1, 2, 3]));
+
+
+let listToArray = list => {
+    let array = [];
+    for (let node = list; node; node = node.rest) {
+        array.push(node.value);
+    }
+    return array;
+}
+
+
+function prepend(value, list) {
+    return {value, rest: list};
+}
+
+
+function nth(list, n) {
+    if(!list) return undefined;
+    else if (n == 0) return list.value;
+    else return nth(list.rest, n - 1);
+}
+
+console.log(arrayToList([10, 20]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 1));
+
+
+
+
+// EXERCISE: DEEP COMPARISON
+
